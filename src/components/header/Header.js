2668fe66b686800a9cce183818a44c06e13c9012
ThapananKulab <nicekrubma123@gmail.com>
2024-04-05 23:20:39 +0700

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import './Header.css' // Ensure your styles are properly imported
 import {
   AiOutlineHome,
   AiOutlineBook,
@@ -8,8 +7,25 @@ import {
   AiOutlineMessage,
 } from 'react-icons/ai'
 import { FaBriefcase } from 'react-icons/fa'
+import { HiMoon, HiSun } from 'react-icons/hi' // Icons for toggling themes
+import './Header.css'
 
-function Header() {
+function Header({ theme, setTheme }) {
+  // Toggle theme and optionally force a refresh
+  const changeTheme = (e) => {
+    e.preventDefault()
+    const newTheme =
+      theme.name === 'light' ? { name: 'dark' } : { name: 'light' }
+    setTheme(newTheme)
+    localStorage.setItem('theme', newTheme.name)
+
+    // Refresh the page to apply theme changes across all components
+    // Note: This is not recommended in a single-page application.
+    window.location.reload()
+  }
+
+  const themeIcon = theme.name === 'light' ? <HiMoon /> : <HiSun />
+
   return (
     <nav>
       <NavLink to="/home" activeClassName="active">
@@ -27,6 +43,9 @@ function Header() {
       <NavLink to="/contact" activeClassName="active">
         <AiOutlineMessage />
       </NavLink>
+      <div onClick={changeTheme} className="theme-toggle-link">
+        {themeIcon}
+      </div>
     </nav>
   )
 }
